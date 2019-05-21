5fba3626b64ea764dd90e7396f4fef200f2492a5
mdboticano <mdboticano@gmail.com>
2019-05-20 17:52:31 -0700

    Updates

---
+++ precache-manifest.2b695ed899f3301be9e8151562ff0b92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c34273c207d884d6243c39fea8c32b",
+    "url": "/frank-ocean-lyrics/index.html"
+  },
+  {
+    "revision": "0472f361cfb15e5e49ea",
+    "url": "/frank-ocean-lyrics/static/css/main.64c944d1.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/frank-ocean-lyrics/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "0472f361cfb15e5e49ea",
+    "url": "/frank-ocean-lyrics/static/js/main.c4513422.chunk.js"
+  },
+  {
+    "revision": "82310dc38bee71e18f2d",
+    "url": "/frank-ocean-lyrics/static/js/runtime~main.d1cd1bb1.js"
+  }
+]);
